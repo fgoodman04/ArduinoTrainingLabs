8916cc733eaea552aa7037b295d321aa28a8ddc3
Faith <02faith.goodman@gulllakecs.org>
2019-11-14 14:15:24 -0500

    Add Initial Program for digitalRead()

--- sketch_nov06a/sketch_nov06a.ino
+++ sketch_nov06a/sketch_nov06a.ino
@@ -6,15 +6,23 @@
  * result to the serial monitor
  */
 
-
-
-
+//digital pin 2 has a push button attached to it
+int pushButton = 2;
 
 void setup() {
-  
+  //initializes serial communication at 9600 bits per second
+  Serial.begin(9600);
+  //make the pushButton pin an input
+  pinMode(pushButton, INPUT);
 
 }
 
 void loop() {
+  //read the input pin
+  int buttonState = digitalRead(pushButton);
+  //prints out the state of the button
+  Serial.println(buttonState);
+  delay(250);
+  //delay in between readings for stability
   
 }
