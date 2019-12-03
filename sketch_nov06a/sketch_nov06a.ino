f318d0ca38c38ada107e981b62d455171bb09e34
Faith <02faith.goodman@gulllakecs.org>
2019-12-03 14:19:03 -0500

    Potientiometer Working from 0-1023

--- sketch_nov06a/sketch_nov06a.ino
+++ sketch_nov06a/sketch_nov06a.ino
@@ -1,7 +1,7 @@
 
 /* Programmer: Faith Goodman
    Date: 12.3.19
-   Program: Potientometer
+   Program: Potientiometer
 
    AnalogReadSerial
    Reads an analog pinput on pin A0, prints to the serial monitor
@@ -9,11 +9,18 @@
 
 
 void setup() {
-
-
+  // Initialize the serial communication at 9600 bits per second
+  Serial.begin(9600);
 }
 
 void loop() {
+  // Read the input on analog pin 0
+  int potientiometerValue = analogRead(A0);
 
-  
+  // Print the value of the potientiometer
+  Serial.println(potientiometerValue);
+
+  //Add a delay for stability
+  delay(1);
+
 }
