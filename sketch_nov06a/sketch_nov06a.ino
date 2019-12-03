2ed5702c87a144fb3d702ea58fce67a461b32aac
Faith <02faith.goodman@gulllakecs.org>
2019-12-03 14:07:31 -0500

    Initial Potientiometer Program

--- sketch_nov06a/sketch_nov06a.ino
+++ sketch_nov06a/sketch_nov06a.ino
@@ -1,50 +1,19 @@
 
 /* Programmer: Faith Goodman
-   Date: 11.6.19
-   Program: Blink
-   This program will turn on an LED for a certain
-   amount of time, turn it off, and back on in a While Loop
+   Date: 12.3.19
+   Program: Potientometer
 
-  Merged 12.2.19
-
-   Programmer: Faith Goodman
-   Date: 11.14.19
-   Program: Digital Read and Serial Port
-   This program will read a digital input on pin 2, prints the
-   result to the serial monitor
+   AnalogReadSerial
+   Reads an analog pinput on pin A0, prints to the serial monitor
 */
-
-//intitialize the LED to Pin 13
-
-int led = 13;
-// Digital pin 2 has a push button attached to it
-int pushButton = 2;
 
 
 void setup() {
-  // Using the function pinMode to call the variable led and provide Output
-  pinMode(led, OUTPUT);
 
-  // Initializes serial communication at 9600 bits per second
-  Serial.begin(9600);
-  //make the pushButton pin an input
-  pinMode(pushButton, INPUT);
 
 }
 
 void loop() {
 
-  // Read the input pin
-  int buttonState = digitalRead(pushButton);
-  //prints out the state of the button
-  Serial.println(buttonState);
-  if (buttonState == HIGH) {
-    digitalWrite(led, HIGH);
-  }
-  else {
-    digitalWrite(led, LOW);
-  }
-
-  delay(1);
-  //delay in between readings for stability
+  
 }
