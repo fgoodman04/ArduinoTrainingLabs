15c8dd52a596b8343867d487a6c33f2c349acc0b
Faith <02faith.goodman@gulllakecs.org>
2019-12-10 13:49:18 -0500

    Add Voltage Read

--- sketch_nov06a/sketch_nov06a.ino
+++ sketch_nov06a/sketch_nov06a.ino
@@ -1,10 +1,13 @@
 
 /* Programmer: Faith Goodman
-   Date: 12.3.19
+   Date: 12/3/19
    Program: Potientiometer
-
+   
    AnalogReadSerial
    Reads an analog pinput on pin A0, prints to the serial monitor
+   
+   Update: 12/10/19
+   Add serial read to be able to read voltages from 0 to 5 volts
 */
 
 
@@ -17,8 +20,11 @@ void loop() {
   // Read the input on analog pin 0
   int potientiometerValue = analogRead(A0);
 
+  // Convert the analog read from 0-1023 to 0-5 volts
+  float voltage = potientiometerValue * (5.0 / 1023.0);
+
   // Print the value of the potientiometer
-  Serial.println(potientiometerValue);
+  Serial.println(voltage);
 
   //Add a delay for stability
   delay(1);
